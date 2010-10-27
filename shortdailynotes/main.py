73b407542dc35953c8604f29403fef9f5b6be55f
Jifeng Zhang <zjfroot@gmail.com>
2010-10-27 16:18:15 +0200

    Added functionality to sort notes to 3 categories. Modifed css to show better look and feel

--- shortdailynotes/main.py
+++ shortdailynotes/main.py
@@ -4,6 +4,7 @@
 #
 #
 import os,sys
+from datetime import datetime, timedelta, date
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
@@ -20,15 +21,28 @@ class MainHandler(webapp.RequestHandler):
         if user:
             if user.email() == "zjfroot@gmail.com":
                 #get word list
-                query = db.GqlQuery("SELECT * FROM Note ORDER BY created DESC LIMIT 20")
+                query = db.GqlQuery("SELECT * FROM Note ORDER BY created DESC LIMIT 30")
                 notes = query.fetch(20)
+                notes_today = []
+                notes_yesterday = []
+                notes_older = []
+                for note in notes:
+                    diff = (date.today() - note.created.date())
+                    if diff == timedelta (days = 0):
+                        notes_today.append(note)
+                    elif diff == timedelta (days = 1):
+                        notes_yesterday.append(note)
+                    else:
+                        notes_older.append(note)
                 
                 author = 'Jifeng Zhang'
                 action = 'programming'
                 template_values = {
                     'author':author,
                     'action':action,
-                    'notes':notes,
+                    'notes_today':notes_today,
+                    'notes_yesterday':notes_yesterday,
+                    'notes_older':notes_older,
                 }
                 path = os.path.join(os.path.dirname(__file__),'index.html')
                 self.response.out.write(template.render(path,template_values))
