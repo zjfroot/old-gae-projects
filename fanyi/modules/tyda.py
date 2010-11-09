ab57c7c8d45b877d26207f81eae7dedcc8ddd9b1
Jifeng Zhang <zjfroot@gmail.com>
2010-11-09 16:09:45 +0100

    Added functionality of parsing tyda html page. Added functionality to translate the english words parsed from tyda to chinese using google translate api.

--- fanyi/modules/tyda.py
+++ fanyi/modules/tyda.py
@@ -13,17 +13,21 @@ from dbmodels import *
 
 class TydaHandler(webapp.RequestHandler):
     def get(self):
+        original_word = self.request.get('src')
         #url = "http://tyda.se/search?form=1&w=krasslig"
-        url = "http://tyda.se/search/krasslig"
+        url = "http://tyda.se/search/"+original_word
         
         self.response.headers['Content-Type'] = 'text/plain'
         try:
             response = urllib2.urlopen(url)
             result = response.read()
-            p = re.compile('.*<a id="tyda_transR\d.* href="/search/.*')
-            m = p.match(result)
+            m = re.findall('a id="tyda_transR\d+" href="/search/\w+',result)
+            words = []
+            for s in m:
+                word = s.split('"')[3].split('/')[2]
+                words.append(word)
             
-            self.response.out.write(m.group(0))
+            self.response.out.write(words)
         except urllib2.URLError, e:
             self.response.out.write("error")
         
--- python/parseTyda/parse.py
+++ python/parseTyda/parse.py
@@ -6,11 +6,11 @@
 import urllib2
 import re
 
-url = "http://tyda.se/search/tystl%C3%A5ten"
+url = "http://tyda.se/search/utvalda"
 
 response = urllib2.urlopen(url)
 result = response.read()
 
-m = re.findall('a id="tyda_transR\d" href="/search/\w+',result)
+m = re.findall('a id="tyda_transR\d+" href="/search/\w+',result)
 for s in m:
     print s.split('"')[3].split('/')[2]
