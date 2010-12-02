ae761d22ff1ba37db0bae5132fc4dbc969bcca9f
Jifeng Zhang <zjfroot@gmail.com>
2010-12-02 14:18:55 +0100

    Added a python tool to count the lines of code in a project. Test results for some projects

---
+++ python/codeLinesCounter/count.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+from os.path import join, getsize
+#path = 'P:/trunk/pan'
+path = 'P:/branches/selenium-grid-1.1'
+fileSubfix = '.java'
+#path = 'I:/oneview/dev/java/src'
+#path = 'H:/'
+totalLinesNr = 0
+totalFileNr = 0
+for root, dirs, files in os.walk(path):
+   #print root, files
+   if '.settings' in dirs:
+                       dirs.remove('.settings')  # don't visit eclipse directories
+   if '.svn' in dirs:
+                       dirs.remove('.svn')  # don't visit svn directories
+   for name in files:
+        if name.endswith(fileSubfix):
+            output = os.popen("wc -l '"+join(root,name) +"'").read().split()
+            totalLinesNr = totalLinesNr + int(output[0])
+            totalFileNr = totalFileNr + 1
+            print output, totalLinesNr, totalFileNr
+
+print 'Total lines:', totalLinesNr
+print 'Total files processed:',totalFileNr
